e89e1815c26f59c8958497f057365f212f12e692
Toan Pham <toan.eliyan@gmail.com>
2024-02-19 16:14:51 -0800

    misc updates

--- get_sym.py
+++ get_sym.py
@@ -3,6 +3,13 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import sys
 
+def get_current_stock_price(stock):
+    # Fetch data 
+    #stock      = yf.Ticker(ticker_symbol)
+    today_data = stock.history(period='1d')
+    current_price = today_data['Close'][0]
+    return current_price
+
 # Check if a stock ticker symbol was provided as and command-line argument
 if len(sys.argv) < 2:
     sys.exit("Usage: {sys.argv[0]} TICKER")
@@ -10,14 +17,30 @@ if len(sys.argv) < 2:
 # Define the stock ticker symbol
 ticker_symbol = sys.argv[1]
 
+
+
 # Get the stock data
-sym_data = yf.Ticker(ticker_symbol)
+stock = yf.Ticker(ticker_symbol)
+
+current_price = get_current_stock_price(stock)
 
 # Fetch historical data for NVIDIA
-hist = sym_data.history(period="1mo")  # 1 year of data
+hist = stock.history(period="1mo")  # 1 year of data
 
 # Display the data
 print(hist)
+
+#options
+options = stock.options
+# Display options
+for exp_date in options: 
+    print(exp_date)
+    option_chain = stock.option_chain(exp_date)
+    #calls = option_chain.calls
+    #print(calls)
+    puts  = option_chain.puts
+    #print(puts)
+
 
 # Plot the closing prices
 if 0:
@@ -29,3 +52,5 @@ if 0:
    plt.xticks(rotation=45)
    plt.grid(True)
    plt.show()
+
+print(f"Current stock price of {ticker_symbol}: ${current_price:.2f}")
